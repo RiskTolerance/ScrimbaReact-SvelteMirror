042d018919f5f7d40e198780b59c6a8383b60be9
Isaac Druin <isaac.druin@gmail.com>
2023-12-12 16:04:51 -0600

    tailwind typo >:|

--- src/lib/components/projects/ProjectListItemDisabled.svelte
+++ src/lib/components/projects/ProjectListItemDisabled.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <li
-	class="flex flex-col sm:flex-row bg-neutral-950 p-5 rounded-md items-center justify-center sm:justify-betweenen"
+	class="flex flex-col sm:flex-row bg-neutral-950 p-5 rounded-md items-center justify-center sm:justify-between"
 >
 	<div class="flex justify-center sm:justify-start mb-2 sm:mb-0 opacity-25">
 		<p class="text-svelte-orange">{name} (WIP)</p>
